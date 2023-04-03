fcb7fe525be181619cd4cde317269d5f1eee20fd
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-03 16:35:04 -0400

    stub up first exercise

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+// Exercise 1
+// The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
+
+// Inside main(), add a statement which prints Hello someName!, with your name replacing someName. Make sure to end the statement with a semicolon.
+
+// For example, if your name were “Maria,” the program would print Hello Maria!.
+
+public class HelloYou {
+    public void main(someName) {
+        
     }
 }
