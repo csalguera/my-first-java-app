f3071c8e3bba96159c12e3c51363e7dd8d8f06e2
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:24:55 -0400

    nest comments inside of main method

--- src/HelloYou.java
+++ src/HelloYou.java
@@ -1,13 +1,13 @@
 public class HelloYou {
-  // The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
-
-  // Inside main(), add a statement which prints Hello someName!, with your name replacing someName. Make sure to end the statement with a semicolon.
-
-  // For example, if your name were “Maria,” the program would print Hello Maria!.
   public static void main(String[] args) {
+    // The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
+
+    // Inside main(), add a statement which prints Hello someName!, with your name replacing someName. Make sure to end the statement with a semicolon.
+
+    // For example, if your name were “Maria,” the program would print Hello Maria!.
     System.out.println("Hello Carlos!");
+    // System is a built-in Java class
+    // out refers to 'output'
+    // println is short for 'print line'
   }
-  // System is a built-in Java class
-  // out refers to 'output'
-  // println is short for 'print line'
 }
