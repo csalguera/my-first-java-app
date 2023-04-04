daca539e73448ef920e58472e6e04509fceb7481
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:21:57 -0400

    create separate java file and class for LanguageFacts

--- src/App.java
+++ src/App.java
@@ -1,51 +1,23 @@
 public class App {
     public static void main(String[] args) {
-        // Exercise 3 -------------------------------------------------------------------------------------------------
-        // The file Timeline.java has plain text information about Java.
-        
-        // Plain text facts aren’t valid syntax. We’ll use comments to avoid breaking the program.
-        
-        // Use the single-line comment syntax for the first fact.
-        
-        // Change this line into a comment:
-        
-        // Sun Microsystems announced the release of Java in 1995
-        
-        System.out.println("Hello Java!");
-        
-        System.out.println("You were born in 1995");
-        
-        // Sun Microsystems announced the release of Java in 1995
-        
-        System.out.println("You were created by James Gosling");
-        
-        /*
-		James Gosling is a Canadian engineer who 
-		created Java while working at Sun Microsystems. 
-		His favorite number is the square root of 2!
-         */
-        
-        System.out.println("You are a fun language!");
-        // ------------------------------------------------------------------------------------------------------------
-
         // Exercise 4 -------------------------------------------------------------------------------------------------
-        // // The LanguageFacts.java file prints information about Java to the screen.
+        // The LanguageFacts.java file prints information about Java to the screen.
         
-        // // Unfortunately, the writer of the file has avoided using whitespace.
+        // Unfortunately, the writer of the file has avoided using whitespace.
         
-        // // Make the file easier to read by adding a newline after each statement!
+        // Make the file easier to read by adding a newline after each statement!
         
-        // System.out.println("Java is a class-based language.");
-        // System.out.println("Java classes have a 'main' method.");
-        // System.out.println("Java statements end with a semicolon.");
+        System.out.println("Java is a class-based language.");
+        System.out.println("Java classes have a 'main' method.");
+        System.out.println("Java statements end with a semicolon.");
         
-        // // Inside main(), add a new statement printing how you feel about coding.
+        // Inside main(), add a new statement printing how you feel about coding.
         
-        // // Start the message with: “Programming is… “.
+        // Start the message with: “Programming is… “.
         
-        // // Remember to place a semicolon at the end of the statement!
+        // Remember to place a semicolon at the end of the statement!
         
-        // System.out.print("Programming is awesome!");
+        System.out.print("Programming is awesome!");
         // ------------------------------------------------------------------------------------------------------------
 
         // Exercise 5 -------------------------------------------------------------------------------------------------
