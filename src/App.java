d70ccff32b6e6d13255e8070a861a42a1f190d44
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:01:10 -0400

    stub up exercise 3

--- src/App.java
+++ src/App.java
@@ -12,22 +12,47 @@ public class App {
         // out refers to 'output'
         // println is short for 'print line'
         // ------------------------------------------------------------------------------------------------------------
-        
+
         // Exercise 2 -------------------------------------------------------------------------------------------------
         // Inside main() and underneath the print statement System.out.println("Let's play hide and seek.");, output the following two statements using System.out.print():
         
         // "Three..."
         // "Two..."
-
+        
         System.out.println("Let's play hide and seek");
         System.out.print("Three...");
         System.out.print("Two...");
         // Underneath your previous statements, output the following two text values using System.out.println():
-
+        
         // "One..."
         // "Ready or not, here I come!"
         System.out.println("One...");
         System.out.println("Ready or not, here I come!");
+        // ------------------------------------------------------------------------------------------------------------
+
+        // Exercise 3 -------------------------------------------------------------------------------------------------
+        // The file Timeline.java has plain text information about Java.
+        
+        // Plain text facts aren’t valid syntax. We’ll use comments to avoid breaking the program.
+        
+        // Use the single-line comment syntax for the first fact.
+        
+        // Change this line into a comment:
+        
+        // Sun Microsystems announced the release of Java in 1995
+        System.out.println("Hello Java!");
+        
+        System.out.println("You were born in 1995");
+
+        Sun Microsystems announced the release of Java in 1995
+        
+        System.out.println("You were created by James Gosling");
+    
+		James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2!
+        
+        System.out.println("You are a fun language!");
         // ------------------------------------------------------------------------------------------------------------
     }
 }
