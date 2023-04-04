7bd38bf1987eba765420acc0efceb19f77ef502a
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:38:09 -0400

    stub up Compiling.java file

---
+++ src/Compiling.java
@@ -0,0 +1,17 @@
+public class Compiling {
+  public static void main(String[] args) {
+    // Let’s practice compiling and executing a file by entering commands in the terminal!
+
+    // Our text editor contains a broken program so we can see how compilers help us catch mistakes. Don’t make any corrections!
+
+    // In the terminal, type this command: javac Compiling.java and press enter or return.
+
+    // Then click the Check Work button to check your work and move on to the next checkpoint.
+
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.")
+
+    System.out.println("Programming is... fun!");
+  }
+}
