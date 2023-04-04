e61e2c4d1db9ee1f59c6803324ea9fec2590dce5
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:07:57 -0400

    stub up exercise 4

--- src/App.java
+++ src/App.java
@@ -40,15 +40,15 @@ public class App {
         // // Change this line into a comment:
         
         // // Sun Microsystems announced the release of Java in 1995
-
+        
         // System.out.println("Hello Java!");
         
         // System.out.println("You were born in 1995");
-
+        
         // // Sun Microsystems announced the release of Java in 1995
         
         // System.out.println("You were created by James Gosling");
-
+        
         // /*
 		// James Gosling is a Canadian engineer who 
 		// created Java while working at Sun Microsystems. 
@@ -56,6 +56,16 @@ public class App {
         //  */
         
         // System.out.println("You are a fun language!");
+        // ------------------------------------------------------------------------------------------------------------
+
+        // Exercise 4 -------------------------------------------------------------------------------------------------
+        // The LanguageFacts.java file prints information about Java to the screen.
+
+        // Unfortunately, the writer of the file has avoided using whitespace.
+
+        // Make the file easier to read by adding a newline after each statement!
+
+        System.out.println("Java is a class-based language.");System.out.println("Java classes have a 'main' method.");System.out.println("Java statements end with a semicolon.");
         // ------------------------------------------------------------------------------------------------------------
     }
 }
