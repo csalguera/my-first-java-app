8ec388523f1ed8ae02360b567a5ccf135774ad3f
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:20:36 -0400

    create separate java file and class for Timeline

---
+++ src/Timeline.java
@@ -0,0 +1,29 @@
+public class Timeline {
+  public static void main(String[] args) {
+    // The file Timeline.java has plain text information about Java.
+    
+    // Plain text facts aren’t valid syntax. We’ll use comments to avoid breaking the program.
+    
+    // Use the single-line comment syntax for the first fact.
+    
+    // Change this line into a comment:
+    
+    // Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("Hello Java!");
+    
+    System.out.println("You were born in 1995");
+    
+    // Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("You were created by James Gosling");
+  
+    /*
+    James Gosling is a Canadian engineer who 
+    created Java while working at Sun Microsystems. 
+    His favorite number is the square root of 2!
+    */
+
+    System.out.println("You are a fun language!");
+  }
+}
