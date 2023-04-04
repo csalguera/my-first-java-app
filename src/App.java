a7031caae57022a6cfb5a35098a12938c3eea27b
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:04:04 -0400

    comment out print statements

--- src/App.java
+++ src/App.java
@@ -1,61 +1,61 @@
 public class App {
     public static void main(String[] args) {
         // Exercise 1 -------------------------------------------------------------------------------------------------
-        // The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
+        // // The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
         
-        // Inside main(), add a statement which prints Hello someName!, with your name replacing someName. Make sure to end the statement with a semicolon.
+        // // Inside main(), add a statement which prints Hello someName!, with your name replacing someName. Make sure to end the statement with a semicolon.
         
-        // For example, if your name were “Maria,” the program would print Hello Maria!.
+        // // For example, if your name were “Maria,” the program would print Hello Maria!.
 
-        System.out.println("Hello Carlos!");
-        // System is a built-in Java class
-        // out refers to 'output'
-        // println is short for 'print line'
+        // System.out.println("Hello Carlos!");
+        // // System is a built-in Java class
+        // // out refers to 'output'
+        // // println is short for 'print line'
         // ------------------------------------------------------------------------------------------------------------
 
         // Exercise 2 -------------------------------------------------------------------------------------------------
-        // Inside main() and underneath the print statement System.out.println("Let's play hide and seek.");, output the following two statements using System.out.print():
+        // // Inside main() and underneath the print statement System.out.println("Let's play hide and seek.");, output the following two statements using System.out.print():
         
-        // "Three..."
-        // "Two..."
+        // // "Three..."
+        // // "Two..."
         
-        System.out.println("Let's play hide and seek");
-        System.out.print("Three...");
-        System.out.print("Two...");
-        // Underneath your previous statements, output the following two text values using System.out.println():
+        // System.out.println("Let's play hide and seek");
+        // System.out.print("Three...");
+        // System.out.print("Two...");
+        // // Underneath your previous statements, output the following two text values using System.out.println():
         
-        // "One..."
-        // "Ready or not, here I come!"
-        System.out.println("One...");
-        System.out.println("Ready or not, here I come!");
+        // // "One..."
+        // // "Ready or not, here I come!"
+        // System.out.println("One...");
+        // System.out.println("Ready or not, here I come!");
         // ------------------------------------------------------------------------------------------------------------
 
         // Exercise 3 -------------------------------------------------------------------------------------------------
-        // The file Timeline.java has plain text information about Java.
+        // // The file Timeline.java has plain text information about Java.
         
-        // Plain text facts aren’t valid syntax. We’ll use comments to avoid breaking the program.
+        // // Plain text facts aren’t valid syntax. We’ll use comments to avoid breaking the program.
         
-        // Use the single-line comment syntax for the first fact.
+        // // Use the single-line comment syntax for the first fact.
         
-        // Change this line into a comment:
+        // // Change this line into a comment:
         
-        // Sun Microsystems announced the release of Java in 1995
-        
-        System.out.println("Hello Java!");
-        
-        System.out.println("You were born in 1995");
+        // // Sun Microsystems announced the release of Java in 1995
 
-        // Sun Microsystems announced the release of Java in 1995
+        // System.out.println("Hello Java!");
         
-        System.out.println("You were created by James Gosling");
+        // System.out.println("You were born in 1995");
 
-        /*
-		James Gosling is a Canadian engineer who 
-		created Java while working at Sun Microsystems. 
-		His favorite number is the square root of 2!
-         */
+        // // Sun Microsystems announced the release of Java in 1995
         
-        System.out.println("You are a fun language!");
+        // System.out.println("You were created by James Gosling");
+
+        // /*
+		// James Gosling is a Canadian engineer who 
+		// created Java while working at Sun Microsystems. 
+		// His favorite number is the square root of 2!
+        //  */
+        
+        // System.out.println("You are a fun language!");
         // ------------------------------------------------------------------------------------------------------------
     }
 }
