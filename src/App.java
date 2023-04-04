cf3b812798bfa5ff1658624e7176a6c8b02f715c
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-04 15:18:03 -0400

    create separate java file and class for HideAndSeek

--- src/App.java
+++ src/App.java
@@ -1,33 +1,20 @@
 public class App {
     public static void main(String[] args) {
-        // Exercise 1 -------------------------------------------------------------------------------------------------
-        // The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
-        
-        // Inside main(), add a statement which prints Hello someName!, with your name replacing someName. Make sure to end the statement with a semicolon.
-        
-        // For example, if your name were “Maria,” the program would print Hello Maria!.
-
-        System.out.println("Hello Carlos!");
-        // System is a built-in Java class
-        // out refers to 'output'
-        // println is short for 'print line'
-        // ------------------------------------------------------------------------------------------------------------
-
         // Exercise 2 -------------------------------------------------------------------------------------------------
-        // // Inside main() and underneath the print statement System.out.println("Let's play hide and seek.");, output the following two statements using System.out.print():
+        // Inside main() and underneath the print statement System.out.println("Let's play hide and seek.");, output the following two statements using System.out.print():
         
-        // // "Three..."
-        // // "Two..."
+        // "Three..."
+        // "Two..."
         
-        // System.out.println("Let's play hide and seek");
-        // System.out.print("Three...");
-        // System.out.print("Two...");
-        // // Underneath your previous statements, output the following two text values using System.out.println():
+        System.out.println("Let's play hide and seek");
+        System.out.print("Three...");
+        System.out.print("Two...");
+        // Underneath your previous statements, output the following two text values using System.out.println():
         
-        // // "One..."
-        // // "Ready or not, here I come!"
-        // System.out.println("One...");
-        // System.out.println("Ready or not, here I come!");
+        // "One..."
+        // "Ready or not, here I come!"
+        System.out.println("One...");
+        System.out.println("Ready or not, here I come!");
         // ------------------------------------------------------------------------------------------------------------
 
         // Exercise 3 -------------------------------------------------------------------------------------------------
