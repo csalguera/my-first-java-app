1dee6ef2c570ebf3c0d670b58039561650cf5e7c
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-05 13:26:51 -0400

    finish review exercise

--- src/Review.java
+++ src/Review.java
@@ -1,11 +1,21 @@
 public class Review {
+  // The text editor holds an empty file named Review.java. Fill it in!
+  
+  // Define a public class named Review.
+  
+  // Use opening and closing curly braces for the scope of the class.
+  
+  // Remember, no semicolons for classes or methods!
+
+  // This code produces an error because Java programs need a main() method.
+
+  // Define the main() method within the curly braces of the Review class.
+
+  // Inside of the curly braces for the main() method, write The main method executes the tasks of the class as a single-line comment.
+
+  // Below the comment, write a statement that prints the following: My first Java program from scratch!.
   public static void main(String[] args) {
-    // The text editor holds an empty file named Review.java. Fill it in!
-
-    // Define a public class named Review.
-
-    // Use opening and closing curly braces for the scope of the class.
-
-    // Remember, no semicolons for classes or methods!
+    // The main method executes the tasks of the class
+    System.out.println("My first Java program from scratch!");
   }
 }
